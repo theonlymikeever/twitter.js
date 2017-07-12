35106c4fbb7c44838634538595856b21b315365a
rivermanvr <vrgithub@riversconsulting.com>
2017-07-11 21:49:44 -0400

    did a simple route

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require( 'express' );
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res, next) => {
+   res.send('Hello world');
+})
+
+app.listen(port, () => { console.log(`listening on port ${port}`) });
